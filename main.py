805eb788b527a7a322852e4197cb80dcb31318fd
wordboxx <yabokonton@gmail.com>
2024-03-30 00:15:21 -0500

    formatted to pep8

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import logging
 import utils.downloader as downloader
 import utils.file_management as file_management
 import utils.total_soyjaks as total_soyjaks
-import logging
 
 logging.basicConfig(level=logging.INFO)
 
@@ -14,18 +14,21 @@ if __name__ == '__main__':
         maximum_soyjaks = total_soyjaks.get_max_soyjak_id()
     # If the request fails, log the error and exit
     except AttributeError:
-        logging.error('Failed to get maximum soyjak ID, booru.soy may be down.')
+        logging.error(
+            'Failed to get maximum soyjak ID, booru.soy may be down.')
         logging.info('Exiting...')
         exit(1)
 
-    post_id = file_management.count_local_soyjaks(file_management.default_directory)
+    post_id = file_management.count_local_soyjaks(
+        file_management.default_directory)
 
     # Download all soyjaks
     while post_id <= maximum_soyjaks:
-        logging.info(f'Getting soyjak {post_id} / {maximum_soyjaks}...')
+        logging.info('Getting soyjak %s / %s...', post_id, maximum_soyjaks)
         try:
             src, variant = downloader.get_image_info(post_id)
-            downloader.download_image(src, post_id, variant, file_management.default_directory)
+            downloader.download_image(
+                src, post_id, variant, file_management.default_directory)
         except TypeError:
             logging.error('Failed to download image')
             logging.info('Skipping...')
